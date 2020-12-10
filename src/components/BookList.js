006c3f78f50c3bcbd093d08d0854750efd85264c
Heltzel <heltzel.service@hotmail.com>
2020-12-10 15:15:11 +0100

    Clean up comments

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,27 +1,6 @@
-// import React, { Componentt } from 'react'
 import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 import { BookContext } from '../contexts/BookContext'
-// export default class BookList extends Component {
-//   static contextType = ThemeContext
-
-//   render() {
-//     const { isLightTheme, light, dark } = this.context
-//     const theme = isLightTheme ? light : dark
-//     return (
-//       <div
-//         className="book-list"
-//         style={{ background: theme.bg, color: theme.syntax }}
-//       >
-//         <ul>
-//           <li style={{ background: theme.ui }}>the way of kings</li>
-//           <li style={{ background: theme.ui }}>the way of the wind</li>
-//           <li style={{ background: theme.ui }}> the final empire</li>
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
 
 export default function BookList() {
   const { books } = useContext(BookContext)
@@ -34,9 +13,6 @@ export default function BookList() {
       style={{ background: theme.bg, color: theme.syntax }}
     >
       <ul>
-        {/* <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the way of the wind</li>
-        <li style={{ background: theme.ui }}> the final empire</li> */}
         {books.map((book) => {
           return (
             <li style={{ background: theme.ui }} key={book.id}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,3 @@
-//
-
 import React, { useContext } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 import { ThemeContext } from '../contexts/ThemeContext'
