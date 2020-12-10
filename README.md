13e8935bcf2fb5b2a8b76f50cfd099dc3ca2db11
Heltzel <heltzel.service@hotmail.com>
2020-12-10 14:40:10 +0100

    Lesson 14 : Multiple contexts using hooks

--- README.md
+++ README.md
@@ -9,15 +9,8 @@ Learn more about the React context api and React hooks.
 ## Lessons:
 
 <ol>
-<li>Introduction</li>
-<li>What is the context API</li>
-<li>Adding a Context and Provider</li>
-<li>Accessing Context part 1 (use the static contextType)</li>
-<li>Accessing Context part 2 (use the Consumer)</li>
-<li>Updating Context data</li>
-<li>Creating multiple contexts</li>
-<li>Consuming multiple contexts</li>
-<li>Intro hooks</li>
+<li>Hooks and Context</li>
+<li>Multiple contexts using Hooks</li>
 </ol>
 
 ***
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import { ThemeContext } from '../contexts/ThemeContext'
+
+// export default class ThemeToggle extends Component {
+//   static contextType = ThemeContext
+//   render() {
+//     const { toggleTheme } = this.context
+//     return (
+//       <button onClick={toggleTheme} className="btn">
+//         Toggle Theme
+//       </button>
+//     )
+//   }
+// }
+
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-export default class ThemeToggle extends Component {
-  static contextType = ThemeContext
-  render() {
-    const { toggleTheme } = this.context
-    return (
-      <button onClick={toggleTheme} className="btn">
-        Toggle Theme
-      </button>
-    )
-  }
+export default function ThemeToggle() {
+  const { toggleTheme } = useContext(ThemeContext)
+  return (
+    <button onClick={toggleTheme} className="btn">
+      Toggle Theme
+    </button>
+  )
 }
