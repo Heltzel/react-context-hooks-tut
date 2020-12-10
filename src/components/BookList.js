98210ae7f13bd7092f3b645cafe0ecd24632aa12
Heltzel <heltzel.service@hotmail.com>
2020-12-10 13:57:23 +0100

    Lesson 13 : Hooks and Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react'
+// import React, { Componentt } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-export default class BookList extends Component {
-  static contextType = ThemeContext
+// export default class BookList extends Component {
+//   static contextType = ThemeContext
 
-  render() {
-    const { isLightTheme, light, dark } = this.context
-    const theme = isLightTheme ? light : dark
-    return (
-      <div
-        className="book-list"
-        style={{ background: theme.bg, color: theme.syntax }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the way of the wind</li>
-          <li style={{ background: theme.ui }}> the final empire</li>
-        </ul>
-      </div>
-    )
-  }
+//   render() {
+//     const { isLightTheme, light, dark } = this.context
+//     const theme = isLightTheme ? light : dark
+//     return (
+//       <div
+//         className="book-list"
+//         style={{ background: theme.bg, color: theme.syntax }}
+//       >
+//         <ul>
+//           <li style={{ background: theme.ui }}>the way of kings</li>
+//           <li style={{ background: theme.ui }}>the way of the wind</li>
+//           <li style={{ background: theme.ui }}> the final empire</li>
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+export default function BookList() {
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the way of the wind</li>
+        <li style={{ background: theme.ui }}> the final empire</li>
+      </ul>
+    </div>
+  )
 }
